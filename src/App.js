9c7425c7305109f3f8257a5a058bb8a0a86685ac
BuraYu <burak.yueksel@hotmail.com>
2024-07-08 19:02:57 +0200

    add skeleton files and darkmode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [colorScheme, setColorScheme] = useState("light");
+
+  const changeColorScheme = () => {
+    colorScheme === "light" ? setColorScheme("dark") : setColorScheme("light");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={`container ${colorScheme}`}>
+        <h1>This is heading!</h1>
+        <p>lorem50 failed me hard.</p>
+        <label class="switch" onChange={changeColorScheme}>
+          <input type="checkbox" />
+          <span class="slider round"></span>
+        </label>{" "}
+      </div>
     </div>
   );
 }
